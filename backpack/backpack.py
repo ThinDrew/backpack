e030c2f081b7ca08b3c9749aa8473e2c73718f22
Кирилл воропаев <voropaev-2002@inbox.ru>
2022-11-18 16:19:00 +0700

    Use new features

--- backpack/backpack.py
+++ backpack/backpack.py
@@ -1,5 +1,5 @@
 from random import randint
-from functools import reduce
+from functools import *
 class Product:
     def __init__(self, mass, cost):
         self.mass = mass
@@ -12,6 +12,7 @@ def randomProduct(count):
     return arr
 
 def deleteNulDif(arr):
+    
     for i in range(len(arr))[::-1]:
         if arr[i] < 0:
             arr.pop(i)
@@ -32,21 +33,27 @@ def f(currentMas, productList):
     if currentMas >= 0 and currentMas < productList[0].mass:
         return 0
     dif = [None] * len(productList)
-    for i in range(len(productList)):
-        dif[i] = currentMas - productList[i].mass
-    dif = deleteNulDif(dif)
-    #map(lambda x,y: f(x,y), dif, productList)
-    for i in range(len(dif)):
-        dif[i] = f(dif[i], productList)
-    dif = maxCost(dif, productList)
+    #for i in range(len(productList)):
+    #    dif[i] = currentMas - productList[i].mass
+    dif = list(map(lambda x: currentMas - x.mass, productList))
+    #dif = deleteNulDif(dif)
+    dif = list(filter(lambda x: x >= 0, dif))
+    #for i in range(len(dif)):
+    #    dif[i] = f(dif[i], productList)
+    dif = list(map(lambda x, y: int(f(x, productList) + y.cost), dif, productList))
+    #dif = maxCost(dif, productList)
+    dif = max(dif)
+
     return dif
      
 
 def main():
     arr = [Product(5, 9), Product(7, 13), Product(11, 21)]
-    arr = sorted(arr, key = lambda pr: pr.mass)
+    #arr = sorted(arr, key = lambda pr: pr.mass)
+    arr.sort(key= lambda pr: pr.mass)
+
     for i in arr:
         print(i.mass, " ", i.cost)
-    print(f(12, arr))
+    print(f(22, arr))
 
 main()
