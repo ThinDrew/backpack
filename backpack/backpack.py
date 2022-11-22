0ceafdf8d7bddf4758ffbda5a388fee3ebb790b3
Андрей Никоноров <andreynikonorov5@gmail.com>
2022-11-23 00:23:42 +0700

    changed dynamic programming for repeating values

--- backpack/backpack.py
+++ backpack/backpack.py
@@ -6,17 +6,6 @@ class Product:
     def __init__(self, mass, cost):
         self.mass = mass
         self.cost = cost
-    @property
-    @classmethod
-    def getItemMass(self) -> float:
-        return self.mass
-    @classmethod
-    @getItemMass.setter
-    def setItemMas(self, mass1: float) -> None:
-        self.mass = mass1
-
-
-
 
 def randomProduct(count):
     arr = []
@@ -63,46 +52,66 @@ def f(currentMas: float, productList: list) -> float:
 def SearchMaxCost(targetMass: int,productList : list) -> list:
     countProduct = len(productList) + 1
     targetMass += 1
+
     newTable = [None] * countProduct
     for i in range(len(newTable)):
         newTable[i] = [None] * targetMass
+
     for i in range(countProduct):
         newTable[i][0] = 0
     for i in range(targetMass):
         newTable[0][i] = 0
-    for k in range(1,countProduct):
+
+    for k1 in range(1, countProduct):
         for s in range(1, targetMass):
-            if s >= productList[k - 1].mass:
-                newTable[k][s] = max(newTable[k-1][s], newTable[k-1][s-productList[k-1].mass] + productList[k - 1].cost)
-            else:
-                newTable[k][s] = newTable[k-1][s]
+            if s >= productList[k1 - 1].mass:
+                ans = []
+                for k in range(1, k1 + 1):
+                   ans.append(newTable[k1][s-productList[k-1].mass] + productList[k - 1].cost)
+                newTable[k1][s] = max(ans)
+            else:     
+                newTable[k1][s] = newTable[k1-1][s]
     return newTable
 
+#def FindProductIndex(k: int, s: int, newTable: list, prList : list, ans: list):
+#    if newTable[k][s] == 0: 
+#        return
+#    for i in range(k):
+#        print(newTable[k][s - prList[i].mass] + prList[i].cost, " == ", newTable[k][s])
+#        if (newTable[k][s] == newTable[k][s - prList[i].mass] + prList[i].cost):
+#            FindProductIndex(k, s - prList[i].mass, newTable, prList, ans)
+#            ans.append(i)
+        
 def FindProductIndex(k: int, s: int, newTable: list, prList : list, ans: list):
-    if newTable[k][s] == 0: 
-        return
-    if (newTable[k][s] == newTable[k-1][s]):
-        FindProductIndex(k - 1, s, newTable, prList, ans)
-    else:
-        FindProductIndex(k - 1, s - prList[k - 1].mass, newTable, prList, ans)
-        ans.append(k - 1)
 
+    while (newTable[k][s] != 0):
+        for i in range(k):
+            #print(newTable[k][s - prList[i].mass] + prList[i].cost, " == ", newTable[k][s])
+            if (newTable[k][s] == newTable[k][s - prList[i].mass] + prList[i].cost):
+                s -= prList[i].mass
+                ans.append(i)
+                break
 
 def main():
-    s = 20
-    arr = [Product(5, 9), Product(7, 13), Product(11, 21)]
-    #arr = [Product(3, 1), Product(4, 6), Product(5, 4), Product(8, 7), Product(9, 6) ]
-    #arr = sorted(arr, key = lambda pr: pr.mass)
+    s = 24
+    #arr = [Product(5, 9), Product(7, 13), Product(11, 21)]
+    arr = [Product(1, 11), Product(2, 22), Product(4, 44)]
+
     arr.sort(key= lambda pr: pr.mass)
     table = SearchMaxCost(s, arr)
     ans = []
-    FindProductIndex(len(arr), s - 1, table, arr, ans)
     for i in range(len(arr) + 1):
         print(table[i])
+    FindProductIndex(len(arr), s, table, arr, ans)
+    print(ans)
+    summ = 0
+    for i in range(len(ans)):
+        summ += arr[ans[i]].cost
+    print(summ)
 
     #for i in arr:
     #    print(i.mass, " ", i.cost)
-    #print(f(200, arr))
+    #print(f(s, arr))
 
 
 main()
